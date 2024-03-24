22fc062db8110442f8ec7f75e0e91d25905067f4
botchagalupe <botchagalupe@gmail.com>
2024-03-24 10:01:26 -0700

    Update workshop

---
+++ install_conda.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+curl https://repo.anaconda.com/archive/Anaconda3-2023.03-Linux-x86_64.sh -o condainstall.sh
+
+source ~/.bashrc
+
+conda --version
+
+conda create --name myenv python=3.11
+
+conda activate myenv
+
+pip install notebook  # For Jupyter Notebook
+
+jupyter notebook --ip=0.0.0.0 --no-browser --port=8888
+
---
+++ install_docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+sudo yum update -y
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+sudo usermod -aG docker ec2-user
+
+sudo docker run hello-world
+
